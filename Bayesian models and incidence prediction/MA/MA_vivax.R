1af459f9bd47b5a0bb9bcf87bcb338304d6db6e6
Mario-Canon-Ayala <92454877+Mario-Canon-Ayala@users.noreply.github.com>
2021-11-18 17:33:14 -0500

    Update MA_vivax.R

--- Bayesian models and incidence prediction/MA/MA_vivax.R
+++ Bayesian models and incidence prediction/MA/MA_vivax.R
@@ -1,6 +1,6 @@
 ########################################################################
 #                                                                      #
-#      Bayesian models and vivax incidence prediction (MARANHãO)       #
+#      Bayesian models and vivax incidence prediction (MARANHÃ£O)       #
 #                                                                      #
 ########################################################################
 ### Packages
@@ -14,12 +14,12 @@ library(spdep)
 library(INLA)
 INLA:::inla.dynload.workaround()
 
-setwd("C:/Users/Mario Cañon/ownCloud/malaria/Analises Mario/Dados/Artigo malaria Journal/INLA data")
+setwd("~/INLA data")
 load("BRASIL_INLA_mes_v.rdata", verbose=TRUE) #vivax data
 
 #### Maranhao 2100000
 BASE_BRA_STA<-subset(BASE_BRA,as.character(BASE_BRA$muni_cut.geocod_6.j.)<220000 & as.character(BASE_BRA$muni_cut.geocod_6.j.)>209999)
-setwd("C:/Users/Mario Cañon/ownCloud/malaria/Analises Mario/Dados/Artigo malaria Journal/Bayesian models and incidence prediction/MA")
+setwd("~/Bayesian models and incidence prediction/MA")
 MAR <- readOGR("MA", "21MUE250GC_SIR")
 pol <- poly2nb(MAR)
 nb2INLA("MA.graph", pol) 
@@ -54,9 +54,7 @@ formula.4<- Y2 ~ 1 +  f(month, model = "rw2", constr = T, cyclic = T) + f(year, 
 t <- proc.time()
 model.inla.5 <- inla(formula.4,family="poisson",data=BASE_SIMPLES2,E=POP/100000, control.predictor=list(compute=TRUE), control.compute=list(dic=TRUE,cpo=TRUE,waic=TRUE))#,waic=TRUE))
 proc.time()-t
-# SAVE RESULTS
-#save(model.inla.5,file="MA_after_INLA_mes_viv_Ypred2016_2018_simples+spa_month_year_ate_2018.rdata")
-load(file="MA_after_INLA_mes_viv_Ypred2016_2018_simples+spa_month_year_ate_2018.rdata")
+
 pred5<-exp(model.inla.5[["summary.linear.predictor"]][["mean"]]) 
 ############# real incidence vs model prevision
 I0<-data.frame(i=BASE_BRA_STA2$i,Y=BASE_BRA_STA2$Y,Yp5=pred5*BASE_BRA_STA2$POP/100000)
@@ -68,7 +66,7 @@ for (j in 1:192) {
   col<-data.frame(i=j,Y=Y_,Pre=Y5)
   Inci<-rbind(Inci,col)
 }
-save(Inci,file="MA_pre_viv.rdata")
+#save(Inci,file="MA_pre_viv.rdata")
 
 ###########################################################################################
 #                                                                                         #

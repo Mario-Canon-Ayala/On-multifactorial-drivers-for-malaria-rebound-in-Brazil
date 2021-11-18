69367bdfb33c8668955071acd400b5481e50c28d
Mario-Canon-Ayala <92454877+Mario-Canon-Ayala@users.noreply.github.com>
2021-11-18 17:34:29 -0500

    Update MT_falciparum.R

--- Bayesian models and incidence prediction/MT/MT_falciparum.R
+++ Bayesian models and incidence prediction/MT/MT_falciparum.R
@@ -14,11 +14,11 @@ library(spdep)
 library(INLA)
 INLA:::inla.dynload.workaround()
 
-setwd("C:/Users/Mario Cañon/ownCloud/malaria/Analises Mario/Dados/Artigo malaria Journal/INLA data")
+setwd("~/INLA data")
 load("BRASIL_INLA_mes_fal.rdata", verbose=TRUE) #falciparum data
 #### Mato Grosso 5100000
 BASE_BRA_STA<-subset(BASE_BRA,as.character(BASE_BRA$muni_cut.geocod_6.j.)<520000 & as.character(BASE_BRA$muni_cut.geocod_6.j.)>509999)
-setwd("C:/Users/Mario Cañon/ownCloud/malaria/Analises Mario/Dados/Artigo malaria Journal/Bayesian models and incidence prediction/MT")
+setwd("~/Bayesian models and incidence prediction/MT")
 mato <- readOGR("MT", "51MUE250GC_SIR")
 pol <- poly2nb(mato)
 nb2INLA("MT.graph", pol) 
@@ -53,9 +53,7 @@ formula.4<- Y2 ~ 1 +  f(month, model = "rw2", constr = T, cyclic = T) + f(year, 
 t <- proc.time()
 model.inla.5 <- inla(formula.4,family="poisson",data=BASE_SIMPLES2,E=POP/100000, control.predictor=list(compute=TRUE), control.compute=list(dic=TRUE,cpo=TRUE,waic=TRUE))#,waic=TRUE))
 proc.time()-t
-# save results
-# save(model.inla.5,file="MT_after_INLA_mes_fal_Ypred2016_2018_simples+spa_month_year_ate_2018.rdata")
-load(file="MT_after_INLA_mes_fal_Ypred2016_2018_simples+spa_month_year_ate_2018.rdata")
+
 pred5<-exp(model.inla.5[["summary.linear.predictor"]][["mean"]]) 
 ############# real incidence vs model prevision
 I0<-data.frame(i=BASE_BRA_STA2$i,Y=BASE_BRA_STA2$Y,Yp5=pred5*BASE_BRA_STA2$POP/100000)
@@ -67,7 +65,7 @@ for (j in 1:192) {
   col<-data.frame(i=j,Y=Y_,Pre=Y5)
   Inci<-rbind(Inci,col)
 }
-save(Inci,file="MT_pre_fal.rdata")
+#save(Inci,file="MT_pre_fal.rdata")
 
 ###########################################################################################
 #                                                                                         #

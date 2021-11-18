550307a02e9c910ac8c78e5786dcaf6338b5b9a4
Mario-Canon-Ayala <92454877+Mario-Canon-Ayala@users.noreply.github.com>
2021-11-18 17:44:43 -0500

    Update RO_vivax.R

--- Bayesian models and incidence prediction/RO/RO_vivax.R
+++ Bayesian models and incidence prediction/RO/RO_vivax.R
@@ -14,11 +14,11 @@ library(spdep)
 library(INLA)
 INLA:::inla.dynload.workaround()
 
-setwd("C:/Users/Mario Cañon/ownCloud/malaria/Analises Mario/Dados/Artigo malaria Journal/INLA data")
+setwd("~/INLA data")
 load("BRASIL_INLA_mes_v.rdata", verbose=TRUE) #vivax data
 #### Subset Rondonia state
 BASE_BRA_STA<-subset(BASE_BRA,as.character(BASE_BRA$muni_cut.geocod_6.j.)<120000 & as.character(BASE_BRA$muni_cut.geocod_6.j.)>109999)
-setwd("C:/Users/Mario Cañon/ownCloud/malaria/Analises Mario/Dados/Artigo malaria Journal/Bayesian models and incidence prediction/RO")
+setwd("~/Bayesian models and incidence prediction/RO")
 ron <- readOGR("RO", "11MUE250GC_SIR")
 pol <- poly2nb(ron)
 nb2INLA("RO.graph", pol) 
@@ -53,9 +53,7 @@ formula.4<- Y2 ~ 1 +  f(month, model = "rw2", constr = T, cyclic = T) + f(year, 
 t <- proc.time()
 model.inla.5 <- inla(formula.4,family="poisson",data=BASE_SIMPLES2,E=POP/100000, control.predictor=list(compute=TRUE), control.compute=list(dic=TRUE,cpo=TRUE,waic=TRUE))#,waic=TRUE))
 proc.time()-t
-# save INLA results
-#save(model.inla.5,file="RO_after_INLA_mes_viv_Ypred2016_2018_simples+spa_month_year_ate_2018.rdata")
-load(file="RO_after_INLA_mes_viv_Ypred2016_2018_simples+spa_month_year_ate_2018.rdata")
+
 pred5<-exp(model.inla.5[["summary.linear.predictor"]][["mean"]]) 
 ############# real incidence vs model prevision
 I0<-data.frame(i=BASE_BRA_STA2$i,Y=BASE_BRA_STA2$Y,Yp5=pred5*BASE_BRA_STA2$POP/100000)
@@ -72,7 +70,7 @@ for (j in 1:192) {
 
 ###########################################################################################
 #                                                                                         #
-#                                 Error map                                               #
+#                              Difference map                                             #
 #                                                                                         #
 ###########################################################################################
 Error<-data.frame(i=BASE_BRA_STA2$i,ID.area=BASE_BRA_STA2$ID.area.sta,Y=BASE_BRA_STA2$Y,Yp5=pred5*BASE_BRA_STA2$POP/100000,diff=BASE_BRA_STA2$Y-pred5*BASE_BRA_STA2$POP/100000)
